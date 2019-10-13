963bb0a1fb3f942631be82e8446312f35193f501
Joe Hahn <joe.hahn@oracle.com>
2019-10-13 17:32:40 -0500

    exhcange

--- test.py
+++ test.py
@@ -20,7 +20,11 @@ print(cryptocompare.get_historical_price_hour(coins[0], curr='USD', limit=2, exc
 print('================== hacked PRICE DAY ================')
 print(cryptocompare.get_historical_price_day(coins[0]))
 print(cryptocompare.get_historical_price_day(coins[0], curr='USD'))
-print(cryptocompare.get_historical_price_day(coins[1], curr=['EUR','USD','GBP']))
+print(cryptocompare.get_historical_price_day(coins[1], curr=['EUR','USD','GBP'], quiet=False))
+print(cryptocompare.get_historical_price_day(coins[1], curr=['EUR','USD','GBP'], quiet=True))
+print(cryptocompare.get_historical_price_day(coins[0], curr='USD', limit=1, quiet=False))
+print(cryptocompare.get_historical_price_day(coins[0], curr='USD', limit=1, exchange='Coinbase', quiet=False))
+print(cryptocompare.get_historical_price_day(coins[0], curr='USD', limit=1, exchange='Kraken', quiet=False))
 
 print('================== COIN LIST =====================')
 response, err = cryptocompare.get_coin_list()
